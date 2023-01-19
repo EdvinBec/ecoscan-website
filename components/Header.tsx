480263ce0f8e9e81d4cbfb19bf08f50a46bab0de
EDVIN\edvin <edvinbecic16@outlook.com>
2023-01-19 08:31:11 +0100

    add heading and cta

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,22 @@ type Props = {};
 const Header = (props: Props) => {
   return (
     <div className="header">
-      <div className="half"></div>
+      <div className="half">
+        <div className="spacing">
+          <h1>
+            Eco <span>Scan</span>
+          </h1>
+          <p>
+            Introducing our new recycling app that makes recycling easy and
+            efficient!
+          </p>
+          <p>
+            Whether it`s paper, plastic, or glass, our app will provide clear
+            instructions on how to recycle each item.
+          </p>
+          <button className="ctaButton">Read more</button>
+        </div>
+      </div>
       <div className="backgroundImage"></div>
     </div>
   );
